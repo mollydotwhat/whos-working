6e7c9c05c55c23994cbc3a9f250837e9f793c94f
molly <molly.salzberger@gmail.com>
2023-11-05 13:28:15 -0600

    starting file structure. can't find a requirement for tests so I'll think about it after I have the skeleton finished.

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+-- tables needed: 
+
+--     department (id: INT PRIMARY KEY, name: VARCHAR(30) to hold department name), 
+--    role (id: INT PRIMARY KEY, title: VARCHAR(30), salary: DECIMAL, department_id: INT), 
+--    employee (id: INT PRIMARY KEY, first_name: VARCHAR(30), last_name: VARCHAR(30), role_id: INT, manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager))
