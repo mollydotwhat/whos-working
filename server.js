c815c58154baa738f2fc1ef83d1ed764c60791d0
molly <molly.salzberger@gmail.com>
2023-11-20 08:48:28 -0600

    deleted some whitespace. need to check back in notes re:imports here.

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const path = require("path");
-
-const routes = require("./routes");
+// const path = require("path");
 const db = require('./config/connection.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //this is a command-line application. what would i need routes for?
-
-
 
 app.listen(PORT, () => console.log('Now listening'));
