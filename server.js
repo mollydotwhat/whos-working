b5c7dd91088154529e7a7629955d8c8a0ce095fd
molly <molly.salzberger@gmail.com>
2023-11-20 08:45:02 -0600

    found more stuff in the wrong file. this is what happens when you work in multiple projects in the same VSCode window.

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = require("express");
 const path = require("path");
-const app = express();
+
 const routes = require("./routes");
 const db = require('./config/connection.js');
-db;
-// const sequelize = require('./config/connection');
-//PRETTY sure I'm not supposed to use sequelize & models, even though we went over them before this assignment is due. This is the unit 12 assignment, those are for unit 13, etc etc. >:(
-const PORT = process.env.PORT || 3001;
 
-app.use(express.static("public"));
-//actually not sure I need public? maybe adjust later
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,9 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 //     res.sendFile(path.join(__dirname, "public/index.html"))
 // })
 
-//All other routes are directed elsewhere
-app.use("/", routes);
+//this is a command-line application. what would i need routes for?
 
 
-//sequelize boilerplate stuff WOULD go here, wrapping around the app.listen. BUT IT'S NOT FOR THIS ASSIGNMENT. 
+
 app.listen(PORT, () => console.log('Now listening'));
