a111a017d03b6f22adff166ad7e8033b4fa29ff0
molly <molly.salzberger@gmail.com>
2023-11-20 08:45:50 -0600

    cleared up the server file. some more.

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// shouldn't need homepage route
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "public/index.html"))
-// })
+app.use(express.urlencoded({ extended: false }));
 
 //this is a command-line application. what would i need routes for?
 
