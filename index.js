2c69d08c08b26f077d4089ffcb82cbcf0212b188
molly <molly.salzberger@gmail.com>
2023-12-03 22:59:20 -0600

    todos and skeletoning

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//this is a command-line application. what would i need routes for?
+// TO DO: inquirer prompts
 
-app.listen(PORT, () => console.log('Now listening'));
+async function start(){
+
+};
+
+// app.listen(PORT, () => console.log('Now listening'));
